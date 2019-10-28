02dd597a845445868e4f20f9187acd4842d906ad
marvinero <marvinero@gmail.com>
2019-10-27 22:17:40 -0400

    second commit

---
+++ src/app/models/models.ts
@@ -0,0 +1,14 @@
+export interface IEmpresa{
+    id?:number;
+    nombre:string;
+    telefono:string;
+    correo_electronico:string;
+    pagina_web:string;
+    celular:string;
+    logo:string;
+    instagram:string;
+    twitter:string;
+    facebook:string;
+    direccion:string;
+    crated_at?:string;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ServerExternalService } from '../server-external.service';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { IEmpresa } from 'src/app/models/models'
 
 @Component({
   selector: 'app-tab1',
@@ -10,10 +11,11 @@ import { Observable } from 'rxjs';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-  public empresas:any;
+  empresas: IEmpresa[]=[];
+  //public empresas:any;
+  url = 'http://localhost:5959/cardmobile/empresa/';
 
   constructor(public navCtrl:NavController,public server:ServerExternalService, public http:HttpClient) {
-
   }
 
   ngOnInit(){
@@ -27,11 +29,8 @@ export class Tab1Page implements OnInit {
     })
   }
 
-  viewInfoIonic(){
-    let url = 'http://localhost:5959/cardmobile/empresa/1';
-    //let url1 = 'http://localhost:5959/cardmobile/empresa/2';
-    let data: Observable<any> = this.http.get(url);
-   
+  viewInfoIonic(){    
+    let data: Observable<any> = this.http.get(this.url);
      data.subscribe(result=>{
       this.empresas = result;
       console.log(result);
